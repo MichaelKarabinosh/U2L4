493735dd4ef966735cfebf35b09d1931cb337b17
MichaelKarabinosh <michaelk368@nycstudents.net>
2024-09-25 19:19:39 -0400

    Implemented TipCalculator class into program

--- src/Main.java
+++ src/Main.java
@@ -18,22 +18,10 @@ public class Main {
 
         System.out.print("Number of People Paying: ");
         String numPeople = s.nextLine();
-
         int numPeopleConv = Integer.parseInt(numPeople); // numPeople is initialized as an int
-        double tipTotal = (tipPercentConv*initialBillConv); // The total tip is equal to the tip applied multiplied by the bill before any tip
-        double finalBill = (initialBillConv+tipTotal); // The final bill is equal to the bill before any tip plus the tip
-        double tipEachPerson = (tipTotal/numPeopleConv); // The amt each person pays in tips
-        double eachPersonPay = (finalBill/numPeopleConv); // The total amt each person should pay
 
-        System.out.println("----------------------------------------------");
-        System.out.println("Total Bill Per Person: $" + String.format("%.2f", eachPersonPay)); // This stat is displayed first because it is the most useful
-        System.out.println("----------------------------------------------");
-        System.out.println("Final Bill: $" + String.format("%.2f", finalBill)); // This follows total bill per person in terms of importance for the average customer
-        System.out.println("----------------------------------------------");
-        System.out.println("Tip Per Person: $" + String.format("%.2f", tipEachPerson));
-        System.out.println("----------------------------------------------");
-        System.out.println("Total Tip: $" + String.format("%.2f", tipTotal));
-        System.out.println("----------------------------------------------");
+        TipCalculator l = new TipCalculator(initialBillConv, tipPercentConv, numPeopleConv);
+        l.printBill();
 
         s.close(); // Stops the scanner s to prevent memory leak
 
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -9,17 +9,36 @@ public class TipCalculator {
         this.numPeople = numPeople;
     }
 
-    public double tipTotal;{
-        tipTotal = billAmt * percentTip;
-    }
-    public double finalBill;
+    public double calcTipTotal()
+
     {
-        finalBill = billAmt * (1 + percentTip);
+        return billAmt * percentTip;
+    }
+    public double calcFinalBill()
+    {
+        return billAmt * (1 + percentTip);
     }
 
-    public double eachPersonPay;
+    public double calcEachPersonPay()
     {
-    eachPersonPay = finalBill/numPeople;
+        return calcFinalBill()/numPeople;
+    }
+    public double calcTipEachPerson()
+    {
+        return calcTipTotal()/numPeople;
+    }
+
+    public void printBill()
+    {
+        System.out.println("----------------------------------------------");
+        System.out.println("Total Bill Per Person: $" + String.format("%.2f", calcEachPersonPay())); // This stat is displayed first because it is the most useful
+        System.out.println("----------------------------------------------");
+        System.out.println("Final Bill: $" + String.format("%.2f", calcFinalBill())); // This follows total bill per person in terms of importance for the average customer
+        System.out.println("----------------------------------------------");
+        System.out.println("Tip Per Person: $" + String.format("%.2f", calcTipEachPerson()));
+        System.out.println("----------------------------------------------");
+        System.out.println("Total Tip: $" + String.format("%.2f", calcTipTotal()));
+        System.out.println("----------------------------------------------");
     }
 
 }
